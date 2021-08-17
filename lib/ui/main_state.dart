42f1860d84787cba7511f9387c18fd8fc151ba0f
beeerlian <naufal.berlian.99@gmail.com>
2021-08-17 14:56:59 +0700

    makin ngawur

--- lib/ui/main_state.dart
+++ lib/ui/main_state.dart
@@ -8,22 +8,33 @@ import 'package:slicing_ios_article_app/ui/upload_article.dart';
 
 class MainState extends StatelessWidget {
   final User? user;
-  const MainState({ Key? key, required this.user }) : super(key: key);
+  const MainState({Key? key, required this.user}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(stream: StreamState.state.stream ,builder: (context, snapshot){
-      if(snapshot.hasError){
-        return Scaffold(body: Center(child: Text("Stream State Error"),));
-      }
-      else{
-        if(snapshot.data == StateEvent.to_home) return Home(user: user);
-        else if(snapshot.data == StateEvent.to_upload) return UploadPage(user: user as dynamic);
-        else if(snapshot.data == StateEvent.to_signin) return Login();
-        else if (snapshot.data == StateEvent.to_signup) return SignUp();
-        else return CircularProgressIndicator();
-      }
-      
-    });
+    return StreamBuilder(
+        stream: StreamState.state.stream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Scaffold(
+                body: Center(
+              child: Text("Stream State Error"),
+            ));
+          } else {
+            switch (snapshot.data) {
+              case StateEvent.to_home:
+                return Home(user: user);
+              case StateEvent.to_signin:
+                return Login();
+              case StateEvent.to_upload:
+                return UploadPage(user: user as dynamic);
+              case StateEvent.to_signup:
+                return SignUp();
+
+              default:
+                return CircularProgressIndicator();
+            }
+          }
+        });
   }
 }
